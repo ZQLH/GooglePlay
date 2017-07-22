c5c3b474afd3f14ab7c489a84b5da5a4bcafb808
fly <fly@itcast.cn>
2017-07-22 17:55:32 +0800

    Delete some files

--- app/src/main/java/com/itheima/googleplay/adapter/javehaha.java
+++
@@ -1,8 +0,0 @@
-package com.itheima.googleplay.adapter;
-
-/**
- * Created by lala on 2017/7/22.
- */
-
-public class javehaha {
-}
--- app/src/main/java/com/itheima/googleplay/adapter/testNew1.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.googleplay.adapter;
-
-/**
- * Created by lala on 2017/7/22.
- */
-
-public class testNew1 {
-    //this is a test about version control
-}
